1ee61babcda8b97ebc71d58e370f8a46efbef0d4
Carla Cascais <10934522+profcarlacascais@users.noreply.gitub.com>
2023-10-08 18:02:23 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -14,7 +14,12 @@ namespace Calculos
             
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4,2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
-           
+
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit,36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36° Celsius={t1}° Fahrenheit");
+            Console.WriteLine($"100° Fahrenheit={t2}° Celsius");
             Console.ReadKey();
         }
     }
